7d5eaaac1542dd16ad64946d0e58f1c69269aefb
Fran Hrzenjak <fran.hrzenjak@gmail.com>
2014-12-13 17:26:22 +0100

    bug hunting in serial comm code...

--- arduino mega/atmega/serial_functions.ino
+++ arduino mega/atmega/serial_functions.ino
@@ -24,71 +24,65 @@ void serial_loop()
             Serial.print("|");
             Serial.println(calibration.in_progress ? 0 : (dome.cycles_for_degree == 0 ? -1 : 1));
         }
-        else if (serial_comm.data_received.charAt(0) == 'd')  //dome or door
+        else if (serial_comm.data_received == "dg")   //get azimuth
         {
-            if (serial_comm.data_received.charAt(1) == 'g') //get azimuth
+            if (dome.cycles_for_degree == 0)
             {
-                if (dome.cycles_for_degree == 0)
-                {
-                    Serial.println("ec");
-                }
-                else
-                {
-                    Serial.println(dome.azimuth);
-                }
+                Serial.println("ec");
             }
-            else if (serial_comm.data_received.charAt(1) == 's')  //go to defined azimuth
+            else
             {
-                if (dome.cycles_for_degree == 0)
-                {
-                    Serial.println("ec");
-                }
-                else
-                {
-                    String data = serial_comm.data_received.substring(2);
-                    char buf[data.length() + 1];
-                    data.toCharArray(buf, data.length() + 1);
-                    double value = atof(buf);
-                    Serial.println(value);
-                    motor_goto(value);
-                }
-            }
-            else if (serial_comm.data_received.charAt(1) == 'p')   //park dome
-            {
-                if (dome.cycles_for_degree == 0)
-                {
-                    Serial.println("ec");
-                }
-                else
-                {
-                    motor_goto(dome.home_azimuth);
-                }
-            }
-            else if (serial_comm.data_received.charAt(1) == 'o')  //open door
-            {
-                door_open();
-            }
-            else if (serial_comm.data_received.charAt(1) == 'c')  //close door
-            {
-                door_close();
+                Serial.println(dome.azimuth);
             }
         }
-        else if (serial_comm.data_received.charAt(0) == 'h')  //home
+        else if (serial_comm.data_received.substring(0, 2) == "ds")   //go to defined azimuth
         {
-            if (serial_comm.data_received.charAt(1) == 'g') //get home
+            if (dome.cycles_for_degree == 0)
             {
-                Serial.println(dome.home_azimuth);
+                Serial.println("ec");
             }
-            else if (serial_comm.data_received.charAt(1) == 's')  //set home at current position
+            else
             {
-                if (dome.cycles_for_degree == 0)
-                {
-                    Serial.println("ec");
-                }
-                else
-                {
-                    EEPROM_write_home(dome.azimuth);
-                }
+                String data = serial_comm.data_received.substring(2);
+                char buf[data.length() + 1];
+                data.toCharArray(buf, data.length() + 1);
+                double value = atof(buf);
+                Serial.println(value);
+                motor_goto(value);
+            }
+        }
+        else if (serial_comm.data_received == "dp")   //park dome
+        {
+            if (dome.cycles_for_degree == 0)
+            {
+                Serial.println("ec");
+            }
+            else
+            {
+                motor_goto(dome.home_azimuth);
+            }
+        }
+        else if (serial_comm.data_received == "do")   //open door
+        {
+            door_open();
+        }
+        else if (serial_comm.data_received == "dc")   //close door
+        {
+            door_close();
+        }
+        if (serial_comm.data_received == "hg")   //get home
+        {
+            Serial.println(dome.home_azimuth);
+        }
+        else if (serial_comm.data_received == "hs")  //set home at current position
+        {
+            if (dome.cycles_for_degree == 0)
+            {
+                Serial.println("ec");
+            }
+            else
+            {
+                EEPROM_write_home(dome.azimuth);
             }
         }
         else if (serial_comm.data_received == "cs")    //calibration start
